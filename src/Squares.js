88f1e6e9a8bdec494c6f34cbf587bf5174dd5579
Susmithag657 <susmithagullipalli2013@gmail.com>
2022-04-06 23:25:18 +0530

    2 files modified

--- src/Squares.js
+++ src/Squares.js
@@ -6,7 +6,7 @@ return (
   <div className='board'>
     {Array.from(new Array(9),(e,i)=>i+1).map((e,i)=>(<div className={`div${e}`} key={i} onClick={()=>handleClick(i)}> {squares[i]}</div>))}
   </div>
-  <div className='status'>{status}</div>
+  <div className={status.startsWith('Winner')?'status winner':'status'}>{status}</div>
   <button className='reset' onClick={handleRestart}>Reset</button>
   </div>
 )
